97783859d2e4e17a6621fe4ee4fad4b5042ae35f
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-06 13:25:23 -0700

    main method implemented

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Solution sol = new Solution();
+        System.out.println(sol.reverseWords("the sky is blue"));
+        System.out.println(sol.reverseWords("  hello world  "));
+        System.out.println(sol.reverseWords("a good   example"));
     }
 }
